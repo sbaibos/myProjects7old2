c1b69d535ef380dc7586dc25da1716ee0f3137fc
sotiris <sbaibos@gmail.com>
2019-02-25 19:12:17 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./components/login/login.component";
+import {AddProjectComponent} from "./components/add-project/add-project.component";
+import {ListProjectComponent} from "./components/list-project/list-project.component";
+import {EditProjectComponent} from "./components/edit-project/edit-project.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+ { path: 'login', component: LoginComponent },
+  { path: 'add-user', component: AddProjectComponent },
+  { path: 'list-user', component: ListProjectComponent },
+  { path: 'edit-user', component: EditProjectComponent },
+  {path : '', component : ListProjectComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
