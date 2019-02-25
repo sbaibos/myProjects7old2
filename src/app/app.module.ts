d221ffcac29749a40768e35f79865323a8cc2aa0
sotiris <sbaibos@gmail.com>
2019-02-25 18:40:37 +0200

    generate 4 components: create, add edit, list"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { AddProjectComponent } from './components/add-project/add-project.component';
+import { EditProjectComponent } from './components/edit-project/edit-project.component';
+import { ListProjectComponent } from './components/list-project/list-project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AddProjectComponent,
+    EditProjectComponent,
+    ListProjectComponent
   ],
   imports: [
     BrowserModule,
