1981c7fd2b9ae48200ba5ae5a6749c47f483e916
sotiris <sbaibos@gmail.com>
2019-03-05 22:14:09 +0200

    create a working service that gets data from sbaibos.com

--- src/app/components/list-project/list-project.component.ts
+++ src/app/components/list-project/list-project.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { ProjectService } from '../../services/project.service';
+import {ProjectModel} from "../../models/project.model";
+
 
 @Component({
   selector: 'app-list-project',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-project.component.css']
 })
 export class ListProjectComponent implements OnInit {
+  users: Object;
+  myProjects:ProjectModel[];
 
-  constructor() { }
+  
+
+  constructor(private ProjectService: ProjectService) { }
 
   ngOnInit() {
+
+    this.ProjectService.getProjects().subscribe(data => {
+        this.users = data
+       console.log(this.users);
+      }
+    );
+     
+    
+    }
+
+    firstClick() {
+    this.ProjectService.firstClick();
   }
+
+
+    
+  
 
 }
--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+//import {HttpClient} from '@angular/common/http';
+import {PROJECTS} from '../mockfiles/mock-project';
+import {ProjectModel} from "../models/project.model";
+import { HttpClient } from '@angular/common/http'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
+projects: ProjectModel[];
+  //projects = PROJECTS;//projects mockfile
+  
+  firstClick() {
+    return console.log('clicked');
+  }
 
-  constructor() { }
+
+
+  
+
+  constructor(private http: HttpClient) { }
+  configUrl  = "http://sbaibos.com/sotostheme/myprojects.php";
+  //configUrl = 'https://reqres.in/api/users';
+  getProjects(){
+   return this.http.get(this.configUrl);
 }
+
+
+
+
+
+  }
