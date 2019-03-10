533b3f8e8e9e3542dad71ed6b1695163be38e64a
sotiris <sbaibos@gmail.com>
2019-03-10 19:08:59 +0200

    list projects ->details button->edit project, installed module angular-pipes with npm, service gets data from http://sbaibos.com/sotostheme/api/myprojects7

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import {PROJECTS} from '../mockfiles/mock-project';
 import {ProjectModel} from "../models/project.model";
 import { HttpClient } from '@angular/common/http'; 
+import { pureObjectDef } from '@angular/core/src/view';
+
 
 @Injectable({
   providedIn: 'root'
@@ -20,13 +22,18 @@ projects: ProjectModel[];
   
 
   constructor(private http: HttpClient) { }
-  configUrl  = "http://sbaibos.com/sotostheme/myprojects.php";
+  basegUrl  = "http://sbaibos.com/sotostheme/api/myprojects7.php";
   //configUrl = 'https://reqres.in/api/users';
   getProjects(){
-   return this.http.get(this.configUrl);
-}
+   return  this.http.get(this.basegUrl);
+   }
+//that works in an api that takes arguments
 
+// getProjectById(id: number) {
+//   return this.http.get(this.basegUrl + '?projectId=' + id);
+//}
 
+//that works on my current static array
 
 
 
