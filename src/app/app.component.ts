267f303cbf56d423313c8bcd9745daeea9a1cd4e
sotiris <sbaibos@gmail.com>
2019-04-05 19:12:57 +0300

    project works! added menu and logout function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { AuthenticationService } from './services/authentication.service';
+import { User } from './models/user';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myProjects';
+  currentUser: User;
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+}
+
+logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+}
+
+
+
 }
