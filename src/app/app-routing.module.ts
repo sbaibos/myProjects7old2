01a7a1224e15e1fee459b94fe097f0d2f78087d5
sotiris <sbaibos@gmail.com>
2019-04-05 15:08:19 +0300

    routing redirects to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,18 @@ import {AddProjectComponent} from "./components/add-project/add-project.componen
 import {ListProjectComponent} from "./components/list-project/list-project.component";
 import {EditProjectComponent} from "./components/edit-project/edit-project.component";
 import {ProjectDetailsComponent} from "./components/project-details/project-details.component";
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
 
+  { path: '', component: ListProjectComponent, canActivate: [AuthGuard]},
  { path: 'login', component: LoginComponent },
-  { path: 'add-project', component: AddProjectComponent },
-  { path: 'list-project', component: ListProjectComponent },
+ { path: 'list-project', component: ListProjectComponent },
+  { path: 'add-project', component: AddProjectComponent },  
   { path: 'project-details', component: ProjectDetailsComponent },
   { path: 'edit-project', component: EditProjectComponent },
-  {path : '', component : ListProjectComponent}
+  {path : '', component : ListProjectComponent},
+  { path: '**', redirectTo: '' }
 
 ];
 
