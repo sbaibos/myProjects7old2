107e878f1d0e7dd9645ec0deaac3b5e6300d7091
sotiris <sbaibos@gmail.com>
2019-04-05 19:59:47 +0300

    changed project username and password

--- src/app/helpers/fake-backend.ts
+++ src/app/helpers/fake-backend.ts
@@ -9,7 +9,7 @@ import { User } from '../models/user';
 export class FakeBackendInterceptor implements HttpInterceptor {
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const users: User[] = [
-            { id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' }
+            { id: 1, username: 'sbaibos', password: 'myprojects7@gmail.com', firstName: 'Test', lastName: 'User' }
         ];
 
         const authHeader = request.headers.get('Authorization');
